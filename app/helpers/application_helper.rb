cb3ba94a6bab6eb85938eb66d5d2cc3031ed5b1f
arbesfeld <mata@mit.edu>
2012-12-23 22:17:07 -0500

    Add more tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title) 
+		first_title = 'Ruby on Rails Tutorial Sample App'
+		if page_title.empty?
+			first_title
+		else
+			"#{first_title} | #{page_title}"
+		end
+	end
 end
