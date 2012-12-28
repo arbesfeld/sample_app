7b45db7884fb14dfaa1a6aad0f28bfcbe742d33a
arbesfeld <arbesfeld@gmail.com>
2012-12-28 17:58:36 -0500

    Add signout link

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
   it { should be_valid }
 
   describe "when name is not present" do
@@ -59,6 +60,7 @@ describe User do
 
     it { should_not be_valid }
   end
+
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
@@ -92,6 +94,10 @@ describe User do
       it { should_not == user_for_invalid_passowrd }
       specify { user_for_invalid_passowrd.should be_false }
     end
+  end
 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
